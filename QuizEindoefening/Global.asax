d8f85c197e681fa922a0d9f386ea3a68c1c08a6a
davyraes <davy-raes@telenet.be>
2018-03-02 13:16:07 +0100

    Add project files.

---
+++ QuizEindoefening/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuizEindoefening.MvcApplication" Language="C#" %>
---
+++ QuizEindoefening/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizEindoefening.Startup))]
+namespace QuizEindoefening
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
